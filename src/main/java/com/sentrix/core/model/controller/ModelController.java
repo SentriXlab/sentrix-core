9c9ca5f4e0d568a41c0651fb658c1aa570a6acbe
jenny <dpwb12@naver.com>
2026-06-01 18:05:17 +0900

    docs: Swagger API 설명 추가

--- src/main/java/com/sentrix/core/model/controller/ModelController.java
+++ src/main/java/com/sentrix/core/model/controller/ModelController.java
@@ -6,12 +6,15 @@ import com.sentrix.core.model.client.ModelServerClient;
 import com.sentrix.core.model.dto.ModelDiagnoseRequest;
 import com.sentrix.core.model.dto.ModelDiagnoseResponse;
 import com.sentrix.core.model.dto.ModelStatusResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
+@Tag(name = "model-controller", description = "Model Server 연동 API")
 @RestController
 public class ModelController {
 
@@ -26,11 +29,13 @@ public class ModelController {
         this.featureService = featureService;
     }
 
+    @Operation(summary = "sentrix-model-server의 연결 상태를 확인한다.")
     @GetMapping("/api/model/status")
     public ModelStatusResponse getModelStatus() {
         return modelServerClient.getModelStatus();
     }
 
+    @Operation(summary = "현재 model input feature를 이용해 model-server의 진단 API를 테스트 호출한다.")
     @PostMapping("/api/model/diagnose")
     public ModelDiagnoseResponse diagnose() {
         ModelInputFeaturesResponse modelInputFeatures = featureService.getModelInputFeatures();
